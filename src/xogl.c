d16744cf371dc0219156ac1791546f217eeee558
Tarek Sherif <tsherif@gmail.com>
2017-12-26 09:17:35 -0500

    Macros for OpenGL function loading

--- src/xogl.c
+++ src/xogl.c
@@ -5,6 +5,16 @@
 #include <GL/glx.h>
 #include "xogl.h"
 
+//////////////////////////////////
+// func is an OpenGL function, e.g: glBindVertexArray
+// XOGL_GET_PROC(glBindVertexArray) =>
+//     glBindVertexArray = (glBindVertexArrayProc) glXGetProcAddress((const GLubyte *) "glBindVertexArray");
+//     if (!glBindVertexArray) fprintf(stderr, "Unable to get proc glBindVertexArray\n");
+//////////////////////////////////
+#define XOGL_GET_PROC(func)\
+func = (func##Proc) glXGetProcAddress((const GLubyte *) #func);\
+if (!func) fprintf(stderr, "Unable to get proc" #func "\n");\
+
 typedef GLXContext (*glXCreateContextAttribsARBProc)(Display*, GLXFBConfig, GLXContext, Bool, const int*);
 static GLXContext ctx;
 
@@ -57,305 +67,56 @@ int xogl_init(Display* disp, Window win) {
 
     glXMakeCurrent(disp, win, ctx);
 
-    glGenVertexArrays = (glGenVertexArraysProc) glXGetProcAddress((const GLubyte *) "glGenVertexArrays");
-
-    if (!glGenVertexArrays) {
-        fprintf(stderr, "Unable to get proc glGenVertexArrays\n");
-    }
-
-    glBindVertexArray = (glBindVertexArrayProc) glXGetProcAddress((const GLubyte *) "glBindVertexArray");
-
-    if (!glBindVertexArray) {
-        fprintf(stderr, "Unable to get proc glBindVertexArray\n");
-    }
-
-    glGenBuffers = (glGenBuffersProc) glXGetProcAddress((const GLubyte *) "glGenBuffers");
-
-    if (!glGenBuffers) {
-        fprintf(stderr, "Unable to get proc glGenBuffers\n");
-    }
-
-    glBindBuffer = (glBindBufferProc) glXGetProcAddress((const GLubyte *) "glBindBuffer");
-
-    if (!glBindBuffer) {
-        fprintf(stderr, "Unable to get proc glBindBuffer\n");
-    }
-
-    glBufferData = (glBufferDataProc) glXGetProcAddress((const GLubyte *) "glBufferData");
-
-    if (!glBufferData) {
-        fprintf(stderr, "Unable to get proc glBufferData\n");
-    }
-
-    glVertexAttribPointer = (glVertexAttribPointerProc) glXGetProcAddress((const GLubyte *) "glVertexAttribPointer");
-
-    if (!glVertexAttribPointer) {
-        fprintf(stderr, "Unable to get proc glVertexAttribPointer\n");
-    }
-
-    glEnableVertexAttribArray = (glEnableVertexAttribArrayProc) glXGetProcAddress((const GLubyte *) "glEnableVertexAttribArray");
-
-    if (!glEnableVertexAttribArray) {
-        fprintf(stderr, "Unable to get proc glEnableVertexAttribArray\n");
-    }
-
-    glCreateShader = (glCreateShaderProc) glXGetProcAddress((const GLubyte *) "glCreateShader");
-
-    if (!glCreateShader) {
-        fprintf(stderr, "Unable to get proc glCreateShader\n");
-    }
-
-    glShaderSource = (glShaderSourceProc) glXGetProcAddress((const GLubyte *) "glShaderSource");
-
-    if (!glShaderSource) {
-        fprintf(stderr, "Unable to get proc glShaderSource\n");
-    }
-
-    glCompileShader = (glCompileShaderProc) glXGetProcAddress((const GLubyte *) "glCompileShader");
-
-    if (!glCompileShader) {
-        fprintf(stderr, "Unable to get proc glCompileShader\n");
-    }
-
-    glCreateProgram = (glCreateProgramProc) glXGetProcAddress((const GLubyte *) "glCreateProgram");
-
-    if (!glCreateProgram) {
-        fprintf(stderr, "Unable to get proc glCreateProgram\n");
-    }
-
-    glAttachShader = (glAttachShaderProc) glXGetProcAddress((const GLubyte *) "glAttachShader");
-
-    if (!glAttachShader) {
-        fprintf(stderr, "Unable to get proc glAttachShader\n");
-    }
-
-    glLinkProgram = (glLinkProgramProc) glXGetProcAddress((const GLubyte *) "glLinkProgram");
-
-    if (!glLinkProgram) {
-        fprintf(stderr, "Unable to get proc glLinkProgram\n");
-    }
-
-    glUseProgram = (glUseProgramProc) glXGetProcAddress((const GLubyte *) "glUseProgram");
-
-    if (!glUseProgram) {
-        fprintf(stderr, "Unable to get proc glUseProgram\n");
-    }
-
-    glGetShaderiv = (glGetShaderivProc) glXGetProcAddress((const GLubyte *) "glGetShaderiv");
-
-    if (!glGetShaderiv) {
-        fprintf(stderr, "Unable to get proc glGetShaderiv\n");
-    }
-
-    glGetProgramiv = (glGetProgramivProc) glXGetProcAddress((const GLubyte *) "glGetProgramiv");
-
-    if (!glGetProgramiv) {
-        fprintf(stderr, "Unable to get proc glGetProgramiv\n");
-    }
-
-    glGetUniformLocation = (glGetUniformLocationProc) glXGetProcAddress((const GLubyte *) "glGetUniformLocation");
-
-    if (!glGetUniformLocation) {
-        fprintf(stderr, "Unable to get proc glGetUniformLocation\n");
-    }
-
-    glUniform1f = (glUniform1fProc) glXGetProcAddress((const GLubyte *) "glUniform1f");
-
-    if (!glUniform1f) {
-        fprintf(stderr, "Unable to get proc glUniform1f\n");
-    }
-
-    glUniform2f = (glUniform2fProc) glXGetProcAddress((const GLubyte *) "glUniform2f");
-
-    if (!glUniform2f) {
-        fprintf(stderr, "Unable to get proc glUniform2f\n");
-    }
-
-    glUniform3f = (glUniform3fProc) glXGetProcAddress((const GLubyte *) "glUniform3f");
-
-    if (!glUniform3f) {
-        fprintf(stderr, "Unable to get proc glUniform3f\n");
-    }
-
-    glUniform4f = (glUniform4fProc) glXGetProcAddress((const GLubyte *) "glUniform4f");
-
-    if (!glUniform4f) {
-        fprintf(stderr, "Unable to get proc glUniform4f\n");
-    }
-
-    glUniform1i = (glUniform1iProc) glXGetProcAddress((const GLubyte *) "glUniform1i");
-
-    if (!glUniform1i) {
-        fprintf(stderr, "Unable to get proc glUniform1i\n");
-    }
-
-    glUniform2i = (glUniform2iProc) glXGetProcAddress((const GLubyte *) "glUniform2i");
-
-    if (!glUniform2i) {
-        fprintf(stderr, "Unable to get proc glUniform2i\n");
-    }
-
-    glUniform3i = (glUniform3iProc) glXGetProcAddress((const GLubyte *) "glUniform3i");
-
-    if (!glUniform3i) {
-        fprintf(stderr, "Unable to get proc glUniform3i\n");
-    }
-
-    glUniform4i = (glUniform4iProc) glXGetProcAddress((const GLubyte *) "glUniform4i");
-
-    if (!glUniform4i) {
-        fprintf(stderr, "Unable to get proc glUniform4i\n");
-    }
-
-    glUniform1ui = (glUniform1uiProc) glXGetProcAddress((const GLubyte *) "glUniform1ui");
-
-    if (!glUniform1ui) {
-        fprintf(stderr, "Unable to get proc glUniform1ui\n");
-    }
-
-    glUniform2ui = (glUniform2uiProc) glXGetProcAddress((const GLubyte *) "glUniform2ui");
-
-    if (!glUniform2ui) {
-        fprintf(stderr, "Unable to get proc glUniform2ui\n");
-    }
-
-    glUniform3ui = (glUniform3uiProc) glXGetProcAddress((const GLubyte *) "glUniform3ui");
-
-    if (!glUniform3ui) {
-        fprintf(stderr, "Unable to get proc glUniform3ui\n");
-    }
-
-    glUniform4ui = (glUniform4uiProc) glXGetProcAddress((const GLubyte *) "glUniform4ui");
-
-    if (!glUniform4ui) {
-        fprintf(stderr, "Unable to get proc glUniform4ui\n");
-    }
-
-    glUniform1fv = (glUniform1fvProc) glXGetProcAddress((const GLubyte *) "glUniform1fv");
-
-    if (!glUniform1fv) {
-        fprintf(stderr, "Unable to get proc glUniform1fv\n");
-    }
-
-    glUniform2fv = (glUniform2fvProc) glXGetProcAddress((const GLubyte *) "glUniform2fv");
-
-    if (!glUniform2fv) {
-        fprintf(stderr, "Unable to get proc glUniform2fv\n");
-    }
-
-    glUniform3fv = (glUniform3fvProc) glXGetProcAddress((const GLubyte *) "glUniform3fv");
-
-    if (!glUniform3fv) {
-        fprintf(stderr, "Unable to get proc glUniform3fv\n");
-    }
-
-    glUniform4fv = (glUniform4fvProc) glXGetProcAddress((const GLubyte *) "glUniform4fv");
-
-    if (!glUniform4fv) {
-        fprintf(stderr, "Unable to get proc glUniform4fv\n");
-    }
-
-    glUniform1iv = (glUniform1ivProc) glXGetProcAddress((const GLubyte *) "glUniform1iv");
-
-    if (!glUniform1iv) {
-        fprintf(stderr, "Unable to get proc glUniform1iv\n");
-    }
-
-    glUniform2iv = (glUniform2ivProc) glXGetProcAddress((const GLubyte *) "glUniform2iv");
-
-    if (!glUniform2iv) {
-        fprintf(stderr, "Unable to get proc glUniform2iv\n");
-    }
-
-    glUniform3iv = (glUniform3ivProc) glXGetProcAddress((const GLubyte *) "glUniform3iv");
-
-    if (!glUniform3iv) {
-        fprintf(stderr, "Unable to get proc glUniform3iv\n");
-    }
-
-    glUniform4iv = (glUniform4ivProc) glXGetProcAddress((const GLubyte *) "glUniform4iv");
-
-    if (!glUniform4iv) {
-        fprintf(stderr, "Unable to get proc glUniform4iv\n");
-    }
-
-    glUniform1uiv = (glUniform1uivProc) glXGetProcAddress((const GLubyte *) "glUniform1uiv");
-
-    if (!glUniform1uiv) {
-        fprintf(stderr, "Unable to get proc glUniform1uiv\n");
-    }
-
-    glUniform2uiv = (glUniform2uivProc) glXGetProcAddress((const GLubyte *) "glUniform2uiv");
-
-    if (!glUniform2uiv) {
-        fprintf(stderr, "Unable to get proc glUniform2uiv\n");
-    }
-
-    glUniform3uiv = (glUniform3uivProc) glXGetProcAddress((const GLubyte *) "glUniform3uiv");
-
-    if (!glUniform3uiv) {
-        fprintf(stderr, "Unable to get proc glUniform3uiv\n");
-    }
-
-    glUniform4uiv = (glUniform4uivProc) glXGetProcAddress((const GLubyte *) "glUniform4uiv");
-
-    if (!glUniform4uiv) {
-        fprintf(stderr, "Unable to get proc glUniform4uiv\n");
-    }
-
-    glUniformMatrix2fv = (glUniformMatrix2fvProc) glXGetProcAddress((const GLubyte *) "glUniformMatrix2fv");
-
-    if (!glUniformMatrix2fv) {
-        fprintf(stderr, "Unable to get proc glUniformMatrix2fv\n");
-    }
-
-    glUniformMatrix3fv = (glUniformMatrix3fvProc) glXGetProcAddress((const GLubyte *) "glUniformMatrix3fv");
-
-    if (!glUniformMatrix3fv) {
-        fprintf(stderr, "Unable to get proc glUniformMatrix3fv\n");
-    }
-
-    glUniformMatrix4fv = (glUniformMatrix4fvProc) glXGetProcAddress((const GLubyte *) "glUniformMatrix4fv");
-
-    if (!glUniformMatrix4fv) {
-        fprintf(stderr, "Unable to get proc glUniformMatrix4fv\n");
-    }
-
-    glUniformMatrix2x3fv = (glUniformMatrix2x3fvProc) glXGetProcAddress((const GLubyte *) "glUniformMatrix2x3fv");
-
-    if (!glUniformMatrix2x3fv) {
-        fprintf(stderr, "Unable to get proc glUniformMatrix2x3fv\n");
-    }
-
-    glUniformMatrix3x2fv = (glUniformMatrix3x2fvProc) glXGetProcAddress((const GLubyte *) "glUniformMatrix3x2fv");
-
-    if (!glUniformMatrix3x2fv) {
-        fprintf(stderr, "Unable to get proc glUniformMatrix3x2fv\n");
-    }
-
-    glUniformMatrix2x4fv = (glUniformMatrix2x4fvProc) glXGetProcAddress((const GLubyte *) "glUniformMatrix2x4fv");
-
-    if (!glUniformMatrix2x4fv) {
-        fprintf(stderr, "Unable to get proc glUniformMatrix2x4fv\n");
-    }
-
-    glUniformMatrix4x2fv = (glUniformMatrix4x2fvProc) glXGetProcAddress((const GLubyte *) "glUniformMatrix4x2fv");
-
-    if (!glUniformMatrix4x2fv) {
-        fprintf(stderr, "Unable to get proc glUniformMatrix4x2fv\n");
-    }
-
-    glUniformMatrix3x4fv = (glUniformMatrix3x4fvProc) glXGetProcAddress((const GLubyte *) "glUniformMatrix3x4fv");
-
-    if (!glUniformMatrix3x4fv) {
-        fprintf(stderr, "Unable to get proc glUniformMatrix3x4fv\n");
-    }
-
-    glUniformMatrix4x3fv = (glUniformMatrix4x3fvProc) glXGetProcAddress((const GLubyte *) "glUniformMatrix4x3fv");
-
-    if (!glUniformMatrix4x3fv) {
-        fprintf(stderr, "Unable to get proc glUniformMatrix4x3fv\n");
-    }
+    XOGL_GET_PROC(glGenVertexArrays);
+    XOGL_GET_PROC(glBindVertexArray);
+    XOGL_GET_PROC(glGenBuffers);
+    XOGL_GET_PROC(glBindBuffer);
+    XOGL_GET_PROC(glBufferData);
+    XOGL_GET_PROC(glVertexAttribPointer);
+    XOGL_GET_PROC(glEnableVertexAttribArray);
+    XOGL_GET_PROC(glCreateShader);
+    XOGL_GET_PROC(glShaderSource);
+    XOGL_GET_PROC(glCompileShader);
+    XOGL_GET_PROC(glCreateProgram);
+    XOGL_GET_PROC(glAttachShader);
+    XOGL_GET_PROC(glLinkProgram);
+    XOGL_GET_PROC(glUseProgram);
+    XOGL_GET_PROC(glGetShaderiv);
+    XOGL_GET_PROC(glGetProgramiv);
+    XOGL_GET_PROC(glGetUniformLocation);
+    XOGL_GET_PROC(glUniform1f);
+    XOGL_GET_PROC(glUniform2f);
+    XOGL_GET_PROC(glUniform3f);
+    XOGL_GET_PROC(glUniform4f);
+    XOGL_GET_PROC(glUniform1i);
+    XOGL_GET_PROC(glUniform2i);
+    XOGL_GET_PROC(glUniform3i);
+    XOGL_GET_PROC(glUniform4i);
+    XOGL_GET_PROC(glUniform1ui);
+    XOGL_GET_PROC(glUniform2ui);
+    XOGL_GET_PROC(glUniform3ui);
+    XOGL_GET_PROC(glUniform4ui);
+    XOGL_GET_PROC(glUniform1fv);
+    XOGL_GET_PROC(glUniform2fv);
+    XOGL_GET_PROC(glUniform3fv);
+    XOGL_GET_PROC(glUniform4fv);
+    XOGL_GET_PROC(glUniform1iv);
+    XOGL_GET_PROC(glUniform2iv);
+    XOGL_GET_PROC(glUniform3iv);
+    XOGL_GET_PROC(glUniform4iv);
+    XOGL_GET_PROC(glUniform1uiv);
+    XOGL_GET_PROC(glUniform2uiv);
+    XOGL_GET_PROC(glUniform3uiv);
+    XOGL_GET_PROC(glUniform4uiv);
+    XOGL_GET_PROC(glUniformMatrix2fv);
+    XOGL_GET_PROC(glUniformMatrix3fv);
+    XOGL_GET_PROC(glUniformMatrix4fv);
+    XOGL_GET_PROC(glUniformMatrix2x3fv);
+    XOGL_GET_PROC(glUniformMatrix3x2fv);
+    XOGL_GET_PROC(glUniformMatrix2x4fv);
+    XOGL_GET_PROC(glUniformMatrix4x2fv);
+    XOGL_GET_PROC(glUniformMatrix3x4fv);
+    XOGL_GET_PROC(glUniformMatrix4x3fv);
 
     return 0;
 }
--- src/xogl.h
+++ src/xogl.h
@@ -3,162 +3,74 @@
 
 #include <GL/gl.h>
 
+//////////////////////////////////
+// ret is return value of OpenGL function
+// func is an OpenGL function, e.g: glBindVertexArray
+// XOGL_DECLARE_PROC(void, glBindVertexArray, GLsizei, GLuint *) =>
+//     typedef void (*glBindVertexArrayProc)(GLsizei, GLuint *);
+//     glBindVertexArrayProc glBindVertexArray;
+//////////////////////////////////
+#define XOGL_DECLARE_PROC(ret, func, ...)\
+typedef ret (*func##Proc)(__VA_ARGS__);\
+func##Proc func;\
+
 // VBOs
-typedef void (*glGenBuffersProc)(GLsizei, GLuint *);
-glGenBuffersProc glGenBuffers;
-
-typedef void (*glBindBufferProc)(GLenum target, GLuint); 
-glBindBufferProc glBindBuffer;
-
-typedef void (*glBufferDataProc)(GLenum target, GLsizeiptr size, const GLvoid *data, GLenum usage); 
-glBufferDataProc glBufferData;
-
+XOGL_DECLARE_PROC(void, glGenBuffers, GLsizei, GLuint *);
+XOGL_DECLARE_PROC(void, glBindBuffer, GLenum target, GLuint);
+XOGL_DECLARE_PROC(void, glBufferData, GLenum target, GLsizeiptr size, const GLvoid *data, GLenum usage);
 
 // VAOs
-typedef void (*glGenVertexArraysProc)(GLsizei, GLuint *);
-glGenVertexArraysProc glGenVertexArrays;
-
-typedef void (*glBindVertexArrayProc)(GLuint); 
-glBindVertexArrayProc glBindVertexArray;
-
-typedef void (*glVertexAttribPointerProc)(GLuint index, GLint size, GLenum type, GLboolean normalized, GLsizei stride, const GLvoid * pointer); 
-glVertexAttribPointerProc glVertexAttribPointer;
-
-typedef void (*glEnableVertexAttribArrayProc)(GLuint index); 
-glEnableVertexAttribArrayProc glEnableVertexAttribArray;
-
+XOGL_DECLARE_PROC(void, glGenVertexArrays, GLsizei, GLuint *);
+XOGL_DECLARE_PROC(void, glBindVertexArray, GLuint);
+XOGL_DECLARE_PROC(void, glVertexAttribPointer, GLuint index, GLint size, GLenum type, GLboolean normalized, GLsizei stride, const GLvoid * pointer);
+XOGL_DECLARE_PROC(void, glEnableVertexAttribArray, GLuint index);
 
 // Programs
-typedef GLuint (*glCreateShaderProc)(GLenum type); 
-glCreateShaderProc glCreateShader;
-
-typedef void (*glShaderSourceProc)(GLuint shader, GLsizei count, const GLchar* const *string, const GLint *length); 
-glShaderSourceProc glShaderSource;
-
-typedef void (*glCompileShaderProc)(GLuint shader); 
-glCompileShaderProc glCompileShader;
-
-typedef void (*glAttachShaderProc)(GLuint program, GLuint shader); 
-glAttachShaderProc glAttachShader;
-
-typedef void (*glLinkProgramProc)(GLuint program); 
-glLinkProgramProc glLinkProgram;
-
-typedef void (*glUseProgramProc)(GLuint program); 
-glUseProgramProc glUseProgram;
-
-typedef GLuint (*glCreateProgramProc)(void); 
-glCreateProgramProc glCreateProgram;
-
-typedef void (*glGetShaderivProc)(GLuint, GLenum, GLint *);
-glGetShaderivProc glGetShaderiv;
-
-typedef void (*glGetProgramivProc)(GLuint, GLenum, GLint *);
-glGetProgramivProc glGetProgramiv;
+XOGL_DECLARE_PROC(GLuint, glCreateShader, GLenum type);
+XOGL_DECLARE_PROC(void, glShaderSource, GLuint shader, GLsizei count, const GLchar* const *string, const GLint *length);
+XOGL_DECLARE_PROC(void, glCompileShader, GLuint shader);
+XOGL_DECLARE_PROC(void, glAttachShader, GLuint program, GLuint shader);
+XOGL_DECLARE_PROC(void, glLinkProgram, GLuint program);
+XOGL_DECLARE_PROC(void, glUseProgram, GLuint program);
+XOGL_DECLARE_PROC(GLuint, glCreateProgram, void);
+XOGL_DECLARE_PROC(void, glGetShaderiv, GLuint, GLenum, GLint *);
+XOGL_DECLARE_PROC(void, glGetProgramiv, GLuint, GLenum, GLint *);
 
 // Uniforms
-typedef GLuint (*glGetUniformLocationProc)(GLuint, const GLchar*);
-glGetUniformLocationProc glGetUniformLocation;
-
-typedef void (*glUniform1fProc)(GLint location, GLfloat v0);
-glUniform1fProc glUniform1f;
- 
-typedef void (*glUniform2fProc)(GLint location, GLfloat v0, GLfloat v1);
-glUniform2fProc glUniform2f;
- 
-typedef void (*glUniform3fProc)(GLint location, GLfloat v0, GLfloat v1, GLfloat v2);
-glUniform3fProc glUniform3f;
- 
-typedef void (*glUniform4fProc)(GLint location, GLfloat v0, GLfloat v1, GLfloat v2, GLfloat v3);
-glUniform4fProc glUniform4f;
- 
-typedef void (*glUniform1iProc)(GLint location, GLint v0);
-glUniform1iProc glUniform1i;
- 
-typedef void (*glUniform2iProc)(GLint location, GLint v0, GLint v1);
-glUniform2iProc glUniform2i;
- 
-typedef void (*glUniform3iProc)(GLint location, GLint v0, GLint v1, GLint v2);
-glUniform3iProc glUniform3i;
- 
-typedef void (*glUniform4iProc)(GLint location, GLint v0, GLint v1, GLint v2, GLint v3);
-glUniform4iProc glUniform4i;
- 
-typedef void (*glUniform1uiProc)(GLint location, GLuint v0);
-glUniform1uiProc glUniform1ui;
- 
-typedef void (*glUniform2uiProc)(GLint location, GLuint v0, GLuint v1);
-glUniform2uiProc glUniform2ui;
- 
-typedef void (*glUniform3uiProc)(GLint location, GLuint v0, GLuint v1, GLuint v2);
-glUniform3uiProc glUniform3ui;
- 
-typedef void (*glUniform4uiProc)(GLint location, GLuint v0, GLuint v1, GLuint v2, GLuint v3);
-glUniform4uiProc glUniform4ui;
- 
-typedef void (*glUniform1fvProc)(GLint location, GLsizei count, const GLfloat *value);
-glUniform1fvProc glUniform1fv;
- 
-typedef void (*glUniform2fvProc)(GLint location, GLsizei count, const GLfloat *value);
-glUniform2fvProc glUniform2fv;
- 
-typedef void (*glUniform3fvProc)(GLint location, GLsizei count, const GLfloat *value);
-glUniform3fvProc glUniform3fv;
- 
-typedef void (*glUniform4fvProc)(GLint location, GLsizei count, const GLfloat *value);
-glUniform4fvProc glUniform4fv;
- 
-typedef void (*glUniform1ivProc)(GLint location, GLsizei count, const GLint *value);
-glUniform1ivProc glUniform1iv;
- 
-typedef void (*glUniform2ivProc)(GLint location, GLsizei count, const GLint *value);
-glUniform2ivProc glUniform2iv;
- 
-typedef void (*glUniform3ivProc)(GLint location, GLsizei count, const GLint *value);
-glUniform3ivProc glUniform3iv;
- 
-typedef void (*glUniform4ivProc)(GLint location, GLsizei count, const GLint *value);
-glUniform4ivProc glUniform4iv;
- 
-typedef void (*glUniform1uivProc)( GLint location, GLsizei count, const GLuint *value);
-glUniform1uivProc glUniform1uiv;
- 
-typedef void (*glUniform2uivProc)( GLint location, GLsizei count, const GLuint *value);
-glUniform2uivProc glUniform2uiv;
- 
-typedef void (*glUniform3uivProc)( GLint location, GLsizei count, const GLuint *value);
-glUniform3uivProc glUniform3uiv;
- 
-typedef void (*glUniform4uivProc)( GLint location, GLsizei count, const GLuint *value);
-glUniform4uivProc glUniform4uiv;
- 
-typedef void (*glUniformMatrix2fvProc)( GLint location, GLsizei count, GLboolean transpose, const GLfloat *value);
-glUniformMatrix2fvProc glUniformMatrix2fv;
- 
-typedef void (*glUniformMatrix3fvProc)( GLint location, GLsizei count, GLboolean transpose, const GLfloat *value);
-glUniformMatrix3fvProc glUniformMatrix3fv;
- 
-typedef void (*glUniformMatrix4fvProc)( GLint location, GLsizei count, GLboolean transpose, const GLfloat *value);
-glUniformMatrix4fvProc glUniformMatrix4fv;
- 
-typedef void (*glUniformMatrix2x3fvProc)(GLint location, GLsizei count, GLboolean transpose, const GLfloat *value);
-glUniformMatrix2x3fvProc glUniformMatrix2x3fv;
- 
-typedef void (*glUniformMatrix3x2fvProc)(GLint location, GLsizei count, GLboolean transpose, const GLfloat *value);
-glUniformMatrix3x2fvProc glUniformMatrix3x2fv;
- 
-typedef void (*glUniformMatrix2x4fvProc)(GLint location, GLsizei count, GLboolean transpose, const GLfloat *value);
-glUniformMatrix2x4fvProc glUniformMatrix2x4fv;
- 
-typedef void (*glUniformMatrix4x2fvProc)(GLint location, GLsizei count, GLboolean transpose, const GLfloat *value);
-glUniformMatrix4x2fvProc glUniformMatrix4x2fv;
- 
-typedef void (*glUniformMatrix3x4fvProc)(GLint location, GLsizei count, GLboolean transpose, const GLfloat *value);
-glUniformMatrix3x4fvProc glUniformMatrix3x4fv;
- 
-typedef void (*glUniformMatrix4x3fvProc)(GLint location, GLsizei count, GLboolean transpose, const GLfloat *value);
-glUniformMatrix4x3fvProc glUniformMatrix4x3fv;
-
+XOGL_DECLARE_PROC(GLuint, glGetUniformLocation, GLuint, const GLchar*);
+XOGL_DECLARE_PROC(void, glUniform1f, GLint location, GLfloat v0);
+XOGL_DECLARE_PROC(void, glUniform2f, GLint location, GLfloat v0, GLfloat v1);
+XOGL_DECLARE_PROC(void, glUniform3f, GLint location, GLfloat v0, GLfloat v1, GLfloat v2);
+XOGL_DECLARE_PROC(void, glUniform4f, GLint location, GLfloat v0, GLfloat v1, GLfloat v2, GLfloat v3);
+XOGL_DECLARE_PROC(void, glUniform1i, GLint location, GLint v0);
+XOGL_DECLARE_PROC(void, glUniform2i, GLint location, GLint v0, GLint v1);
+XOGL_DECLARE_PROC(void, glUniform3i, GLint location, GLint v0, GLint v1, GLint v2);
+XOGL_DECLARE_PROC(void, glUniform4i, GLint location, GLint v0, GLint v1, GLint v2, GLint v3);
+XOGL_DECLARE_PROC(void, glUniform1ui, GLint location, GLuint v0);
+XOGL_DECLARE_PROC(void, glUniform2ui, GLint location, GLuint v0, GLuint v1);
+XOGL_DECLARE_PROC(void, glUniform3ui, GLint location, GLuint v0, GLuint v1, GLuint v2);
+XOGL_DECLARE_PROC(void, glUniform4ui, GLint location, GLuint v0, GLuint v1, GLuint v2, GLuint v3);
+XOGL_DECLARE_PROC(void, glUniform1fv, GLint location, GLsizei count, const GLfloat *value);
+XOGL_DECLARE_PROC(void, glUniform2fv, GLint location, GLsizei count, const GLfloat *value);
+XOGL_DECLARE_PROC(void, glUniform3fv, GLint location, GLsizei count, const GLfloat *value);
+XOGL_DECLARE_PROC(void, glUniform4fv, GLint location, GLsizei count, const GLfloat *value);
+XOGL_DECLARE_PROC(void, glUniform1iv, GLint location, GLsizei count, const GLint *value);
+XOGL_DECLARE_PROC(void, glUniform2iv, GLint location, GLsizei count, const GLint *value);
+XOGL_DECLARE_PROC(void, glUniform3iv, GLint location, GLsizei count, const GLint *value);
+XOGL_DECLARE_PROC(void, glUniform4iv, GLint location, GLsizei count, const GLint *value);
+XOGL_DECLARE_PROC(void, glUniform1uiv,  GLint location, GLsizei count, const GLuint *value);
+XOGL_DECLARE_PROC(void, glUniform2uiv,  GLint location, GLsizei count, const GLuint *value);
+XOGL_DECLARE_PROC(void, glUniform3uiv,  GLint location, GLsizei count, const GLuint *value);
+XOGL_DECLARE_PROC(void, glUniform4uiv,  GLint location, GLsizei count, const GLuint *value);
+XOGL_DECLARE_PROC(void, glUniformMatrix2fv,  GLint location, GLsizei count, GLboolean transpose, const GLfloat *value);
+XOGL_DECLARE_PROC(void, glUniformMatrix3fv,  GLint location, GLsizei count, GLboolean transpose, const GLfloat *value);
+XOGL_DECLARE_PROC(void, glUniformMatrix4fv,  GLint location, GLsizei count, GLboolean transpose, const GLfloat *value);
+XOGL_DECLARE_PROC(void, glUniformMatrix2x3fv, GLint location, GLsizei count, GLboolean transpose, const GLfloat *value);
+XOGL_DECLARE_PROC(void, glUniformMatrix3x2fv, GLint location, GLsizei count, GLboolean transpose, const GLfloat *value);
+XOGL_DECLARE_PROC(void, glUniformMatrix2x4fv, GLint location, GLsizei count, GLboolean transpose, const GLfloat *value);
+XOGL_DECLARE_PROC(void, glUniformMatrix4x2fv, GLint location, GLsizei count, GLboolean transpose, const GLfloat *value);
+XOGL_DECLARE_PROC(void, glUniformMatrix3x4fv, GLint location, GLsizei count, GLboolean transpose, const GLfloat *value);
+XOGL_DECLARE_PROC(void, glUniformMatrix4x3fv, GLint location, GLsizei count, GLboolean transpose, const GLfloat *value);
 
 int xogl_init(Display* disp, Window win);
 void xogl_destroy(Display* disp);
